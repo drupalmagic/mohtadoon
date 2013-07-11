7ed1977aa830d61afd71c278307d734ce53b542a
drupalmagic <drupalmagic@gmail.com>
2013-07-11 10:49:23 +0200

    fix collapsible filters and views

---
+++ sites/all/themes/almohtadoon/page--taxonomy.tpl.php
@@ -0,0 +1,55 @@
+<div id="wrapper">
+  <div id="header">
+  	<div class="logo">
+     <a href="<?php print $front_page; ?>"><img src="<?php print $GLOBALS['base_url']; ?>/sites/all/themes/almohtadoon/images/almohtadoon-logo.png" /></a>
+    </div>      <div class="social-network">
+      	<?php if($page['social_btns']): ?>
+          <?php print render($page['social_btns']); ?>
+        <?php endif; ?>
+      </div>
+      
+      <a href="<?php print url('node/26', array('absolute' => true)); ?>" class="donate"></a>
+      
+      <div id="menu">
+      	<?php print theme('links__system_main_menu', array('links' => $main_menu, 'attributes' => array('id' => 'main-menu', 'class' => array('links', 'inline', 'clearfix')))); ?>
+      </div>
+  </div>
+
+  <div class="quran-banner">
+  <span>
+  	بسم الله الرحمن الرحيم   وَإِنْ يُرِيدُوا أَنْ يَخْدَعُوكَ فَإِنَّ حَسْبَكَ اللَّهُ هُوَ الَّذِي أَيَّدَكَ بِنَصْرِهِ وَبِالْمُؤْمِنِينَ * وَأَلَّفَ بَيْنَ قُلُوبِهِمْ لَوْ أَنْفَقْتَ مَا فِي الْأَرْضِ جَمِيعًا مَا أَلَّفْتَ بَيْنَ قُلُوبِهِمْ وَلَكِنَّ اللَّهَ أَلَّفَ بَيْنَهُمْ إِنَّهُ عَزِيزٌ حَكِيمٌ
+      </span>
+  </div>
+
+  <div class="search-bar">
+    <?php if($page['search']): ?>
+      <?php print render($page['search']); ?>
+    <?php endif; ?>
+  	<form>
+          <input type="text" name="search" class="search-box" />
+          <input type="submit" name="submit" class="search-btn" value=" " />
+      </form>
+      
+      <a href="" class="publishing-story"></a>
+  </div>
+
+  <div id="container">
+     
+    <div class="left-section big-section" style="width: 940px;">
+     	<?php if($page['content']): ?>
+        <?php print render($page['content']); ?>
+      <?php endif; ?>
+    </div>
+      
+  </div>        
+
+  <div style="clear:both; height: 5px;"></div>
+
+  <div id="footer">
+  	<div class="copyrights">
+      <?php if($page['footer']): ?>
+        <?php print render($page['footer']); ?>
+      <?php endif; ?>
+    </div>
+  </div>
+</div>
---
+++ sites/all/themes/almohtadoon/views-view-fields--taxonomy-term--page.tpl.php
@@ -0,0 +1,42 @@
+<?php if($id == 1){ ?>
+
+
+<div class="right-big-section" style="width: 940px; margin-bottom: 20px; margin-right: 20px;">  
+
+<div class="box" style="position: relative; float: left; margin-left: 23px;">
+  <div class="img-place">
+		<a href="<?php print strip_tags($fields['path']->content); ?>">
+			<img src="<?php print image_style_url('220x240-bigthumb', $row->_field_data['nid']['entity']->field_image['und'][0]['uri']);?>" />
+		</a>
+  </div>
+    <div class="title"><?php print strip_tags($fields['title']->content, '<a>'); ?></div>
+    <p><?php print strip_tags($fields['field_manchet']->content); ?></p>
+     <div class="sharing-buttons" style="position: absolute; top: 200px; width: 197px; right: 245px; height: 30px;">
+				<span style="float: right; margin-top: 2px;">
+					<fb:share-button type="button_count" href="<?php print strip_tags($fields['path']->content); ?>"></fb:share-button>
+				</span>
+				<span style="float: right;">
+					<a href="https://twitter.com/share" class="twitter-share-button" data-lang="en" data-url="<?php print strip_tags($fields['path']->content); ?>" data-text="<?php print strip_tags($fields['title']->content) . ' ' . strip_tags($fields['path']->content); ?>" data-via"almohtadon">Tweet</a>
+				</span>
+			</div>
+</div>
+
+</div>
+
+<?php }else{ ?>
+
+<div class="box" style="position: relative;">
+	<div class="img-place"><?php print strip_tags($fields['field_image']->content, '<img>, <a>'); ?></div>
+	<div class="title"><?php print strip_tags($fields['title']->content, '<a>'); ?></div>
+	<p style="width: 150px;"><?php print strip_tags($fields['field_manchet']->content); ?></p>
+	<div class="sharing-buttons" style="position: absolute; top: 105px; width: 197px; right: 55px; height: 30px;">
+		<span style="float: right; margin-top: 2px;">
+			<fb:share-button type="button_count" href="<?php print strip_tags($fields['path']->content); ?>"></fb:share-button>
+		</span>
+		<span style="float: right;">
+			<a href="https://twitter.com/share" class="twitter-share-button" data-lang="en" data-url="<?php print strip_tags($fields['path']->content); ?>" data-text="<?php print strip_tags($fields['title']->content) . ' ' . strip_tags($fields['path']->content); ?>" data-via"almohtadon">Tweet</a>
+		</span>
+	</div>
+</div>
+
+<?php } ?>
