52a7207c6a23bb19606b57d74ef706bb8e390521
drupalmagic <drupalmagic@gmail.com>
2013-07-14 21:27:19 +0200

    news ticker

--- sites/all/themes/almohtadoon/views-view-unformatted--news-ticker--block.tpl.php
+++ sites/all/themes/almohtadoon/views-view-unformatted--news-ticker--block.tpl.php
@@ -1,19 +1,20 @@
-<!--
 <link href="<?php print $GLOBALS['base_url']; ?>/sites/all/themes/almohtadoon/jquery_news_ticker/styles/ticker-style.css" rel="stylesheet" type="text/css" />
 <script src="<?php print $GLOBALS['base_url']; ?>/sites/all/themes/almohtadoon/jquery_news_ticker/includes/jquery.ticker.js" type="text/javascript"></script>
 <script type="text/javascript">
 	(function($){
 		$(function () {
-      $('#js-news').ticker();
+      $('#js-news').ticker({
+      	direction: 'rtl',
+      	controls: false,
+      	titleText: ''
+      });
     });
 	})(jQuery);
-</script> -->
+</script>
 <ul id="js-news" class="js-hidden">
-<marquee behavior="scroll" direction="left">
 	<?php foreach($rows as $id => $row): ?>
 		<?php print $row; ?>
 	<?php endforeach; ?>
-</marquee>
 </ul>
 
 
